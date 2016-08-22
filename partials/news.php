38b43a629e4189272542dae952c1a5435d4a870f
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-08-22 16:14:06 -0400

    Seccion de lectura de noticias

---
+++ partials/news.php
@@ -0,0 +1,125 @@
+<div class="container news-view-container height95pc">
+    <div class="news-view height100pc">
+        <div class="news-container height100pc justScrollY">
+            <div class="news-header">
+                <div class="metadata-container">
+                    <div class="category-container">Noticias > América Latina > Política </div>
+                    <div class="date-author-container"> Publicado por: Autor del Artículo - 14 de junio de 2016, a las 11:45am</div>
+                </div>
+
+                <div class="image-container">
+                    <img src="http://placehold.it/934x314" alt="">
+                </div>
+                <div class="title-container">
+                    <div class="subtitle">Nec semper dui eros mattis ac.</div>
+                    <div class="title">nibh dictum sed a sem in mi facilisi</div>
+                </div>
+            </div>
+
+            <div class="row content">
+                <div class="col-sm-12">
+                    <div class="visualizations pull-right">11.326 visualizaciones</div>
+                </div>
+                <div class="col-sm-9">
+                    <div class="news-content">
+                        <div class="clearfix"></div>
+                        <div class="content" ng-bind-html="onFocusNews.content">
+                            <p>Ligula ut tincidunt eleifend luctus condimentum magna. Condimentum mattis consectetur magna nunc duis lobortis bibendum interdum metus. Eu accumsan vehicula ante nisl mattis nec quis. Morbi lorem risus pellentesque in nec in. Tempus elementum eleifend pellentesque vulputate. Imperdiet et viverra amet mauris vestibulum. Hendrerit sed et ultricies nulla posuere. Congue mauris turpis risus purus arcu felis. Id in euismod et volutpat luctus fusce dapibus tincidunt in. Elit turpis eros dolor elit lacus in. Imperdiet accumsan ac luctus nec.</p>
+                            <p>Dictum lectus sed purus bibendum accumsan. Viverra sed elit nullam leo curabitur gravida. Tempus faucibus diam est in. Vitae varius nulla urna ut vehicula. Sodales nec et mattis ut volutpat posuere in venenatis. At aliquet ipsum quam vestibulum a. Eleifend sit nunc lorem sodales a. Viverra id ultricies nunc augue venenatis bibendum aliquam facilisis. Eleifend curabitur iaculis nullam orci primis dolor ac interdum metus. Et sed eu sit ligula eros nam arcu in quisque. Tempus ligula ipsum nunc feugiat curabitur fusce blandit non. Quisque dolor leo in orci. Quis quis viverra ornare ultricies ornare. Lorem sed tincidunt congue ac in nibh sed. Interdum tincidunt ipsum in et vitae turpis quis dictum et. Velit hendrerit accumsan platea integer ut sodales. Ac bibendum ipsum suscipit imperdiet ornare in.</p>
+                            <p>Condimentum nec sapien turpis et curabitur sit. Bibendum elementum pellentesque lobortis vivamus aliquam at. Imperdiet ac vel aliquam posuere nec magna aliquam dictum. Vitae ultricies ligula elit feugiat. Proin nunc ullamcorper congue massa. At maecenas lacus elit venenatis sed accumsan. Ut turpis in sed id. Elit tincidunt posuere et feugiat quisque aliquam. Mattis morbi pellentesque lectus vulputate. Tempus hendrerit condimentum hendrerit elit nunc quis tincidunt vestibulum at.</p>
+                            <p>Duis eu ultricies pellentesque pretium commodo lacus auctor aliquam. In habitasse varius ullamcorper pretium nibh in lorem vehicula aliquam. Nibh ipsum nullam ligula praesent convallis leo ut sagittis. Justo ac dolor donec nibh. Tempor odio leo ac mauris ipsum vehicula eget. Et mauris dictumst tortor eleifend. Sapien auctor tristique vestibulum sollicitudin nibh augue tortor quis ac. Nec faucibus nunc nec eleifend. Ultricies congue tempus ante nullam tincidunt porttitor sed quis nulla. Varius quis amet vehicula ac tincidunt. Pretium rhoncus a tempor arcu dui.</p>
+                            <p>In eleifend morbi blandit ante lorem leo ullamcorper. Erat odio tellus tempor phasellus at tempus facilisis leo. Ut etiam porttitor tortor augue id. Arcu erat sollicitudin turpis integer congue in orci accumsan varius. Mattis feugiat phasellus elit sed tempus tempor phasellus turpis tellus. Tempor leo orci habitasse nisl. Quis arcu ante erat lobortis vestibulum platea lorem. Ut lobortis vestibulum eleifend elit. Pellentesque dictum libero consectetur amet ut ac fringilla. Ipsum eu sit nunc leo sit at ligula. Nibh pellentesque nisi iaculis ligula semper rutrum ut. Dictum eros elit suscipit orci non porttitor justo nunc. Odio turpis nulla vitae erat orci orci vestibulum accumsan. Elit mattis semper ac faucibus sit donec vehicula. Laoreet vestibulum vitae amet donec elit. Fusce convallis a vel congue. Eget vestibulum nulla facilisis condimentum ligula sit leo orci pharetra. Lorem ipsum ultrices orci in magna orci turpis sed nec. Mauris risus lobortis mattis quis.</p>
+                            <p>Arcu purus nisl suscipit tortor velit neque elit posuere sodales. Tristique quisque tempus facilisis in erat in ultricies urna. Velit dictum sed ultricies dignissim erat sed imperdiet odio nibh. Vestibulum in iaculis odio nec nibh metus ullamcorper in vel. Nisl velit elit lorem sodales elit posuere ac tempor quisque. Varius dignissim ac morbi elit quam platea magna. Interdum fusce ligula fusce sapien. Ipsum quisque at vestibulum nunc lacus nunc. Aliquet quisque nulla ullamcorper rutrum fermentum sit sed ornare. Tincidunt mauris semper nulla purus sed nunc vestibulum.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="share-new">
+                        <span>Comparta esta noticia: </span>
+                        <div class="socials-container">
+                            <img src="http://placehold.it/16x16" alt="" ng-repeat="n in [0,1,2,3,4,5]">
+                        </div>
+                    </div>
+                    <div class="comments-container">
+                        <form role="form">
+                            <div class="form-group">
+                                <textarea type="email" class="form-control" id="email"></textarea>
+                                <span>Para poder comentar, deberá iniciar su sesión en telesurtv.net</span>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <div class="news-container-footer row">
+                <div class="col-sm-1 read-later text-center">
+                    <p><span class="glyphicon glyphicon-asterisk"></span></p>
+                    <p class="text">Leer luego</p>
+                </div>
+                <div class="col-sm-6 more-articles">
+                    <p>Leer mas articulos por: <a href="">Autor del articulo</a></p>
+                    <p>Ver mas articulos de: <a href="">Politica - America Latina - Venezuela</a></p>
+                </div>
+                <div class="col-sm-3 this-article text-right">
+                    <p>Este articulo tiene 23 comentarios</p>
+                    <p>Ha sido compartido 11 veces</p>
+                </div>
+                <div class="col-sm-2 prev-next text-right">
+                    <div class="indicator">
+                        <span class="text">Noticia anterior</span>
+                        <span class="glyphicon glyphicon-circle-arrow-up"></span>
+                    </div>
+                    <div class="indicator">
+                        <span class="text">Proxima noticia</span>
+                        <span class="glyphicon glyphicon-circle-arrow-down"></span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="news-sidebar height100pc justScrollY" >
+
+            <div class="close-window">
+                <span class="glyphicon glyphicon-remove-circle"></span>
+                Cerrar esta ventana
+            </div>
+
+            <div class="news-advertisement">
+                <img src="http://placehold.it/320x105" alt="">
+            </div>
+
+            <div class="news-selector">
+                <div class="selector-header">
+                    Relacionados:
+                </div>
+                <ul>
+
+                    <?php
+                        foreach (range(0, 4) as &$valor) {?>
+                    <li>
+                        <a href="#">Lorem ipsum dolor</a>
+                    </li>
+                    <?}?>
+                </ul>
+            </div>
+            <div class="news-selector">
+                <div class="selector-header">
+                    De su interés:
+                </div>
+                <ul>
+
+                    <?php
+                        foreach (range(0, 4) as &$valor) {?>
+                    <li>
+                        <a href="#">Lorem ipsum dolor</a>
+                    </li>
+                    <?}?>
+                </ul>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+    <div class="news-sidebar ColPruebas"></div>
+
+</div>
+
