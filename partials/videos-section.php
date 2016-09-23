5b0d55d68a8712e0239736aa39a530067b036dc3
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-09-23 13:50:52 -0400

    Implementación de lógica con Jquery para carga dinámica de videos en el home

--- partials/videos-section.php
+++ partials/videos-section.php
@@ -1,23 +1,15 @@
-<div slides-to-show="5" slides-to-scroll="1" class="videos-section">
-  <?php
-  foreach (range(0, 12) as &$valor) {?>
-    <div class="video-item" ng-repeat="news in videoNews">
-      <div class="video-container">
-        <img src="http://placehold.it/242x160" alt="">
-        <!--<div class="menu-container  text-right" ng-click="menuFocusNews = !menuFocusNews">-->
-        <!--<span class="glyphicon glyphicon-option-horizontal"></span>-->
-        <!--<ul  ng-init="menuFocusNews = false"  ng-show="menuFocusNews">-->
-        <!--<li>menu-element</li>-->
-        <!--<li>menu-element</li>-->
-        <!--</ul>-->
-        <!--</div>-->
-      </div>
-      <div class="video-caption">
-        <a href="">
-          Laborum ullamco deserunt consectetur mollit do enim in
-        </a>
-      </div>
-    </div>
-  <?php } ?>
-</div>
+<script src="http://jwpsrv.com/library/HeeyEgeeEeS9bCIAC0MJiQ.js"></script>
+<script defer type="text/javascript" src="js/videos-fn.js"></script>
+<!--<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>-->
+<script>
+ 
+  $( document ).ready(function() {
 
+    $().GetVideos();
+  });
+
+</script>
+<div id="VideosContenedor">
+<!--  <a href="http://videos.telesurtv.net/"><h6>Video <cms:img src="/arte/redes/video.png" scaleType="2" alt="Telesur" /><h5><a href="http://videos.telesurtv.net/">Ver todos</a></h5></h6></a>-->
+  <!--TS_HOME_VIDEO-->
+</div>
