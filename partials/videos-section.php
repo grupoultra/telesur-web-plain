f1b8db6b7888ee94e709882911d29967963b06f5
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-09-23 13:53:25 -0400

    Modificaciones minimas

--- partials/videos-section.php
+++ partials/videos-section.php
@@ -1,15 +1,9 @@
 <script src="http://jwpsrv.com/library/HeeyEgeeEeS9bCIAC0MJiQ.js"></script>
-<script defer type="text/javascript" src="js/videos-fn.js"></script>
-<!--<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>-->
+<script defer type="text/javascript" src="js/videos-fn-2.js"></script>
 <script>
- 
   $( document ).ready(function() {
-
     $().GetVideos();
   });
-
 </script>
 <div id="VideosContenedor">
-<!--  <a href="http://videos.telesurtv.net/"><h6>Video <cms:img src="/arte/redes/video.png" scaleType="2" alt="Telesur" /><h5><a href="http://videos.telesurtv.net/">Ver todos</a></h5></h6></a>-->
-  <!--TS_HOME_VIDEO-->
 </div>
