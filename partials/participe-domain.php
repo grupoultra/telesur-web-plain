04b01679ea16d5d93a8c1fb7e78d6ede2c384595
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-09-26 12:37:48 -0400

    Ajustes responsive en participe

--- partials/participe-domain.php
+++ partials/participe-domain.php
@@ -1,71 +1,88 @@
 <div class="domain-container participe-domain">
-  <div class="participe-section">
-    <img class="participe-image" src="images/ciudadano.svg" alt="Ciudadano teleSUR" width="320" height="70">
+  <div class="row">
 
-    <p>
-      “Ciudadano teleSUR” es una campaña de recaudación de fondos que tiene como objetivo sumar a nuestros seguidores
-      a la tarea de garantizar la democratización de la información y contribuir así con la pluralidad de voces en el
-      concierto internacional.
-    </p>
+  <div class="col-md-4 col-xs-12">
+    <div class="participe-section">
+      <img class="participe-image img-responsive" src="images/ciudadano.svg" alt="Ciudadano teleSUR" width="320" height="70">
 
-    <p>
-      Ser un ciudadano teleSUR te permitirá participar en el financiamiento de nuestra señal a través de un aporte que
-      va desde 1$ hasta la cantidad que decidas. Tu contribución podrá ser como un aporte único o de forma mensual.
-    </p>
+      <p>
+        “Ciudadano teleSUR” es una campaña de recaudación de fondos que tiene como objetivo sumar a nuestros seguidores
+        a la tarea de garantizar la democratización de la información y contribuir así con la pluralidad de voces en el
+        concierto internacional.
+      </p>
 
-    <a href="" class="sublink">Beneficios</a>
-    <a href="" class="sublink">Términos de uso</a>
-    <a href="" class="sublink">Más información</a>
-    <a href="" class="footer">Quiero ser un ciudadano teleSUR</a>
+      <p>
+        Ser un ciudadano teleSUR te permitirá participar en el financiamiento de nuestra señal a través de un aporte que
+        va desde 1$ hasta la cantidad que decidas. Tu contribución podrá ser como un aporte único o de forma mensual.
+      </p>
+
+      <a href="" class="sublink">Beneficios</a>
+      <a href="" class="sublink">Términos de uso</a>
+      <a href="" class="sublink">Más información</a>
+      <a href="" class="footer">Quiero ser un ciudadano teleSUR</a>
+    </div>
   </div>
-  <div class="participe-section">
-    <h2 class="title">Soy reportero</h2>
-    <div class="items-section">
+  <div class="col-md-4 col-xs-12">
+    <div class="participe-section">
+      <h2 class="title">Soy reportero</h2>
+      <div class="items-section">
 
 
-      <?php
-        foreach (range(0, 2) as &$valor) {?>
-      <div class="reporter-item">
-        <img src="http://placehold.it/120x120" alt="">
-        <div class="item-info">
-          <div class="item-title">Lorem ipsum dolor sit amet</div>
-          <div class="metadata">
-            <div class="author pull-left">John Doe</div>
-            <div class="country pull-right">Venezuela</div>
+        <?php
+          foreach (range(0, 2) as &$valor) {?>
+        <div class="reporter-item no-gutter">
+          <div class="item-image col-xs-4">
+            <img class="img-responsive" src="http://placehold.it/120x120" alt="">
           </div>
-        </div>
-        <div class="clearfix"></div>
-      </div>
-      <?php }?>
-    </div>
 
-    <a href="" class="footer">Coviértete en un reportero</a>
+          <div class="item-title col-xs-8">
+            <a href="">Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</a>
+          </div>
+
+          <div class="item-metadata col-xs-8">
+            <div class="author col-xs-6"><a href="">John Doe</a></div>
+            <div class="country col-xs-6"><a href="">Venezuela</a></div>
+          </div>
+
+          <div class="clearfix"></div>
+        </div>
+        <?php }?>
+      </div>
+
+      <a href="" class="footer">Coviértete en un reportero</a>
+    </div>
   </div>
-  <div class="participe-section">
-    <h2 class="title">Blogs teleSUR</h2>
-    <div class="items-section">
+  <div class="col-md-4 col-xs-12">
+    <div class="participe-section">
+      <h2 class="title">Blogs teleSUR</h2>
+      <div class="items-section">
 
-      <?php
-        foreach (range(0, 4) as &$valor) {?>
-      <div class="blog-item">
-        <img src="http://placehold.it/75x75" alt="">
-        <div class="item-info">
-          <div class="item-author"> Lorem ipsum dolor sit amet</div>
-          <ul>
-            <li>Consectetur adipisicing elit. </li>
-            <li>Aliquam culpa dolore dolorum </li>
-          </ul>
-          <div class="perfil pull-right">Perfil del bloguero - Mas artículos</div>
+        <?php
+          foreach (range(0, 4) as &$valor) {?>
+        <div class="blog-item no-gutter">
+          <div class="image-container col-xs-3">
+            <img class="img-responsive" src="http://placehold.it/75x75" alt="">
+          </div>
+          <div class="item-info col-xs-9">
+            <div class="item-author"> Lorem ipsum dolor sit amet</div>
+            <ul>
+              <li>Consectetur adipisicing elit. </li>
+              <li>Aliquam culpa dolore dolorum </li>
+            </ul>
+            <div class="perfil pull-right">Perfil del bloguero - Mas artículos</div>
+          </div>
+          <div class="clearfix"></div>
         </div>
-        <div class="clearfix"></div>
+        <?php }?>
+
       </div>
-      <?php }?>
+
+      <a href="" class="footer">Me gustaría tener un blog en teleSUR</a>
 
     </div>
-
-    <a href="" class="footer">Me gustaría tener un blog en teleSUR</a>
-
   </div>
 
   <div class="clearfix"></div>
+  </div>
+
 </div>
