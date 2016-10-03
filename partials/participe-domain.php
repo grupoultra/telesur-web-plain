44f59d033e9e0357bcd9bf3cc5613ae306a56311
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-10-03 11:39:15 -0400

    Anadiendo contenido del dominio participe

--- partials/participe-domain.php
+++ partials/participe-domain.php
@@ -16,10 +16,153 @@
         va desde 1$ hasta la cantidad que decidas. Tu contribución podrá ser como un aporte único o de forma mensual.
       </p>
 
-      <a href="" class="sublink">Beneficios</a>
-      <a href="" class="sublink">Términos de uso</a>
-      <a href="" class="sublink">Más información</a>
-      <a href="" class="footer">Quiero ser un ciudadano teleSUR</a>
+      <a class="sublink" data-toggle="modal" data-target="#participe-beneficios-content">Beneficios</a>
+      <a class="sublink" data-toggle="modal" data-target="#participe-terminos-content">Términos de uso</a>
+      <a class="sublink" data-toggle="modal" data-target="#participe-mas-content">Más información</a>
+      <a class="sublink" data-toggle="modal" data-target="#participe-quiero-content">Quiero ser un ciudadano teleSUR</a>
+
+      <!-- Modal -->
+      <div class="modal fade" id="participe-beneficios-content" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar"><span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title" id="myModalLabel">Beneficios</h4>
+            </div>
+            <div class="modal-body">
+              <ul>
+                <li>Envío de boletín informativo de teleSUR</li>
+                <li>Carnet que te acredita como Ciudadano teleSUR</li>
+                <li>Invitación a eventos especiales cuando se realicen en tu ciudad</li>
+                <li>Aquellos que sumen cinco amigos a los donativos, recibirán  una franela, remera, como reconocimiento especial</li>
+                <li>Envío de novedades acerca de la programación en forma anticipada</li>
+              </ul>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Modal -->
+      <div class="modal fade" id="participe-terminos-content" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar"><span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title" id="myModalLabel">Términos de uso</h4>
+            </div>
+            <div class="modal-body">
+              <h4>TÉRMINOS Y CONDICIONES</h4>
+              <p>Asegúrese de leer los términos y condiciones indicados a continuación, se entenderán aceptados sin restricciones, reservas o modificaciones por el usuario.
+                Los mismos podrán ser modificados cuando teleSUR lo crea conveniente, si esto sucede, será comunicado mediante una nota en nuestro sitio web, rigiendo a partir de ese momento, las modificaciones realizadas.</p>
+
+              <h3>PRIVACIDAD</h3>
+              <h4>POLÍTICA DE PRIVACIDAD</h4>
+              <p>Nuestra Política de Privacidad tiene como finalidad proteger su información personal de terceros, y ayudarnos a manejarla de la mejor manera para brindarle una mejor experiencia.
+                Asegúrese de leer con atención nuestra Política de Privacidad, ya que se entiende que está de acuerdo y acepta sin ningún tipo de reservas, modificaciones o restricciones inscribirse en nuestro sitio web.</p>
+
+              <h4>¿Qué información nos permite recoger?</h4>
+              <p>Puede navegar a través de la mayoría de las páginas sin revelarnos su identidad o ningún dato personal.</p>
+              <p>Nuestros servidores almacenan archivos de registros anónimos durante las visitas que realizan los usuarios al sitio web de teleSUR. La información recogida es utilizada para elaborar estadísticas, por ejemplo, sobre la hora de acceso al sitio, las páginas web que han remitido al usuario a nuestro sitio, el tipo de navegador que usan nuestros visitantes y las páginas web que visitan.</p>
+              <p>Recolectando esta información podemos determinar el uso que se hace de nuestro sitio web y nos proporciona datos útiles para mejorarlo.</p>
+              <p>teleSUR no recoge información que identifique al visitante de nuestro sitio web, por ejemplo, se mantendrá sin identificar su nombre, dirección, número de teléfono o dirección de correo electrónico, a menos que el visitante decida proporcionarla, haciendo un donativo, escribiendo un posteo, cargando fotos o videos, suscribiéndose a nuestros boletines electrónicos o accediendo a partes restringidas de nuestro sitio web.</p>
+
+              <h4>¿Cómo usamos la información recogida?</h4>
+              <p>Los datos personales recogidos por teleSUR y sus proveedores de servicios, serán almacenados en un entorno seguro, donde serán tratados como “datos confidenciales” y sólo serán utilizados con el objetivo por los cuales fueron recogidos, de acuerdo con lo indicado en nuestro sitio web o según sea requerido para cumplir con nuestras obligaciones legales, por ejemplo, los datos suministrados para recibir boletines electrónicos de teleSUR serán utilizados para dar curso a la solicitud expresada y enviar información periódicamente.</p>
+
+              <h4>• Traspaso de información personal</h4>
+              <p>Los datos personales recogidos no serán en ningún término vendidos, arrendados y/o compartidos con terceros, a menos que nos sea exigido por ley revelar dicha información.</p>
+
+              <h4>• Información recogida, alojamiento y procesamiento</h4>
+              <p>Nuestro sitio web se encuentra alojado en un tercero, un proveedor de servicios, quien por lo tanto, podrá tener acceso y procesar la información de la persona recogida por nosotros, asimismo podemos recurrir a terceros para procesar su información personal, por ejemplo, ese tercero puede procesar sus datos personales para dar curso a las expresiones de interés con ocasión de unirse a teleSUR, tramitar donativos o procesar y enviar información a las personas que decidan suscribirse a nuestros boletines electrónicos. La utilización de su información personal será procesada únicamente en nombre de teleSUR y están obligados por estrictas condiciones de confidencialidad.</p>
+
+              <h4>¿Puede acceder a su información personal?</h4>
+              <p>Si, puede acceder a los datos personales que hemos recogido, con la finalidad de modificarlos u objetar su procesamiento.</p>
+              <p>Si desea objetar su procesamiento u obtener más información sobre nuestra Política de Privacidad, sírvase contactarnos a las siguientes direcciones; Calle Vargas con Calle Santa Clara, edif. teleSUR, Boleíta Norte, Caracas – Venezuela código postal 1070 o envíanos un correo electrónico a contactenos@telesurtv.net.</p>
+              <h4>Consentimiento del usuario</h4>
+              <p>Nos comprometemos a garantizar su privacidad, nuestra Política de Privacidad abarca y rige todas las páginas web creadas por nosotros.</p>
+              <p>Aquellas páginas web de terceros, cuyos enlaces reposan en nuestras páginas web pueden tener sus propias políticas de privacidad.</p>
+
+              <h3 class="center">DERECHO DE PROPIEDAD INTELECTUAL</h3>
+              <h4>TITULARIDAD</h4>
+              <p>teleSUR es titular de los derechos de propiedad intelectual, bien sea por su creación o cesión, derivados de los textos, imágenes, medios y formas de presentación y montajes de sus páginas.
+                Estos derechos son susceptibles de registro y protección según el régimen de la propiedad intelectual en la República Bolivariana de Venezuela y tratados internacionales relativos a la materia.</p>
+              <p>Queda expresamente reconocida la prohibición por parte de los usuarios de reproducir, distribuir, comunicar públicamente y/o transformar, parcial o totalmente el contenido del sitio web.</p>
+
+              <h4>LEGISLACIÓN Y JURISDICCIÓN COMPETENTE</h4>
+              <p>El cumplimiento e incumplimiento de los Términos y Condiciones, está sujeto a el ordenamiento jurídico, jurisprudencia y doctrina de la República Bolivariana de Venezuela, excluyendo expresamente la aplicación de cualquier otro.</p>
+              <p>Para todos los efectos legales provenientes de dudas y/o controversias que pudieran suscitarse luego de aceptados los Términos y Condiciones serán resueltas por los Tribunales de la República Bolivariana de Venezuela, escogiendo como domicilio especial  la Ciudad de Caracas.</p>
+
+              <h4>CONDICIONES DE USO</h4>
+              <p>Nos reservamos el derecho de vetar y/o eliminar a los usuarios que no utilicen el sitio web con fines meramente informativos o solidarios.</p>
+              <p>Las relaciones entre usuarios serán supervisadas de acuerdo a un estricto criterio de respeto.</p>
+
+              <h4>COOKIES</h4>
+              <p>Una cookie es una información sólo de texto que transmite un sitio web al disco duro de su ordenador a fin de que el sitio pueda recordar quién es usted. Normalmente una cookie contendrá el nombre del dominio de Internet del que procede, la fecha de caducidad de la cookie, y un valor, por lo general una cifra única generada aleatoriamente.</p>
+              <p>Se pueden establecer restricciones a las cookies o bloquearlas configurando las opciones del navegador de Internet.</p>
+              <p>Encontrará más información en el siguiente enlace:</p>
+              <a href="http://www.aboutcookies.org/Default.aspx?page=1" target="_blank">http://www.aboutcookies.org/Default.aspx?page=1</a>
+              <br>
+              <br> <p>Se pueden definir las opciones del equipo para aceptar todas las cookies, recibir una notificación cuando se haya enviado una cookie, o nunca recibir cookies. Para impedir que se descarguen cookies o para controlar la forma en que las cookies actúan en su equipo, lea la información de ayuda que le ofrece su navegador de Internet o entre en:</p>
+              <a href="http://www.allaboutcookies.org">http://www.allaboutcookies.org</a>
+
+              <h4>Afiliación</h4>
+              <p>Las donaciones que realice serán a través de Paypal®, por lo que serán regidas bajo sus Términos y Condiciones.</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal fade" id="participe-mas-content" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar"><span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title" id="myModalLabel">Mas información</h4>
+            </div>
+            <div class="modal-body">
+              <p>Para sumarte al equipo de Ciudadanos teleSUR si te encuentras en Venezuela,  una vez que hayas ingresado a ciudadano.telesurtv.net  haz click en el botón Venezuela.</p>
+              <p>Llena los datos solicitados en el formulario como: Nombre, Apellido,  Sexo y tu correo electrónico. Acepta los términos y condiciones y regístrate en nuestra comunidad.</p>
+
+              <h4>Pago por medio de Transferencia</h4>
+              <p><span class="bold">Banco:</span> <br> Cta Corriente Banco de Venezuela</p>
+              <p><span class="bold">Número de cuenta:</span> <br> 01020762280000026217</p>
+              <p><span class="bold">RIF:</span> <br> G-200045000</p>
+              <p><span class="bold">A nombre de:</span> <br> La Nueva Televisión del Sur (T.V SUR) C.A</p>
+
+              <h4>Si te encuentras en otro país puedes sumarte a Ciudadanos teleSUR a través de PayPal.</h4>
+              <p>1- Ingresa a <span class="bold">ciudadano.telesurtv.net</span></p>
+              <p>2- Llena el formulario con tus datos y acepta los términos y condiciones.</p>
+              <p>3- Haz Click en el botón de Pay pal y llena los datos correspondientes para tu afiliación.</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Modal -->
+      <div class="modal fade" id="participe-quiero-content" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar"><span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title" id="myModalLabel">Quiero ser un ciudadano teleSUR</h4>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Modal -->
     </div>
   </div>
   <div class="col-md-4 col-xs-12">
@@ -64,12 +207,8 @@
             <img class="img-responsive" src="http://placehold.it/75x75" alt="">
           </div>
           <div class="item-info col-xs-9">
-            <div class="item-author"> Lorem ipsum dolor sit amet</div>
-            <ul>
-              <li>Consectetur adipisicing elit. </li>
-              <li>Aliquam culpa dolore dolorum </li>
-            </ul>
-            <div class="perfil pull-right">Perfil del bloguero - Mas artículos</div>
+            <div class="item-author"><a href="">Lorem ipsum dolor sit amet</a></div>
+            <div class="item-title"> <a href="">Consectetur adipisicing elit.</a> </div>
           </div>
           <div class="clearfix"></div>
         </div>
